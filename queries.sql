3c8edd4507d8bd1696f55e171eb39379f8b819bb
ciraganenicole <ciraganenicole@gmail.com>
2022-05-31 06:40:23 +0200

    Create queries to select

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name = 'Luna';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+SELECT name FROM animals WHERE neutered=TRUE AND escape_attempts < 3;
+SELECT date_of_birth from animals WHERE name='Agumon' OR name='Pikachu';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+SELECT * from animals WHERE neutered=TRUE;
+SELECT * from animals WHERE name <> 'Gabumon';
+SELECT * from animals WHERE weight_kg >=10.4 and weight_kg <= 17.3;
