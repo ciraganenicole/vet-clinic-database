5f503a227c8ce8721cd7965a0b77d0e8c6205ffd
ciraganenicole <ciraganenicole@gmail.com>
2022-05-31 06:41:07 +0200

    Create table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,name VARCHAR NOT NULL,date_of_birth DATE NOT NULL,escape_attempts INT NOT NULL,neutered 
+BOOL NOT NULL,weight_kg DECIMAL(50,2) NOT NULL
 );
